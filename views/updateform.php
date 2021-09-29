5da5facf633d4e59a941062c7dd9627630d2a951
Jurgen <jurgen@insan3.nl>
2021-09-29 14:19:36 +0200

    test

--- views/updateform.php
+++ views/updateform.php
@@ -39,12 +39,12 @@ $row = mysqli_fetch_array($result);
  </div>
 <div class="field">
 <label>Type:</label> <select name="seq_type">
-<option value="<?php echo $row['seq_type']; ?>">(huidige)</option>
+<option value="<?php echo $row['seq_type']; ?>"><?php echo $row['seq_type']; ?> (huidige)</option>
 <option value="S_RECURRING">Recurring</option>
 <option value="S_FIRST">First</option>
 <option value="S_FINAL">Final</option>
 <option value="S_ONEOFF">Single</option>
-echo '</select>' ;
+</select>
 
  </div>
 <input type="submit" value="Wijzig" />
